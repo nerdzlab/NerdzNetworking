c1870b21c22a8d9dabd5b549ee29dc3b62705754
Vasyl Khmil <vasylkhmil1995@gmail.com>
2023-01-31 12:37:18 +0200

    - will tris work? - who knows, lets try!

--- Sources/NerdzNetworking/Models/Default/RequestRetriers/AuthTokenRetrier.swift
+++ Sources/NerdzNetworking/Models/Default/RequestRetriers/AuthTokenRetrier.swift
@@ -15,14 +15,24 @@ public class AuthTokenRetrier<RequestType: Request>: OnStatusCodesRequestRetrier
     
     public var onNeedRefreshRequest: RequestType?
     
+    private var pendingRefreshRequest: RequestType?
+    
     public init() {
         
+    }
+    
+    public func canHandleError<T>(_ error: ErrorResponse<T.ErrorType>, for request: T) -> Bool where T : Request {
+        let canBeExecutedByParrent = (self as OnStatusCodesRequestRetrier).canHandleError(error, for: request)
+        
+        return canBeExecutedByParrent && (pendingRefreshRequest as AnyObject) === (request as AnyObject)
     }
     
     public func handleError<T>(_ error: ErrorResponse<T.ErrorType>, for request: T, on endpoint: Endpoint) async -> T? where T : Request {
         guard let refreshRequest = onNeedRefreshRequest else {
             return nil
         }
+        
+        pendingRefreshRequest = refreshRequest
         
         if let response = try? await endpoint.asyncExecute(refreshRequest) {
             endpoint.setNewAuthToken(response)
