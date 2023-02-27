8784386d7bddfc24b19fb82bcd42f4da9f03a6da
Vasyl Khmil <vasyl@nerdzlab.com>
2023-02-27 14:43:18 +0200

    - added descriptive decodding error;

--- Sources/NerdzNetworking/Mapping/Mapper.swift
+++ Sources/NerdzNetworking/Mapping/Mapper.swift
@@ -8,16 +8,6 @@
 import Foundation
 
 class Mapper<T: Decodable> {
-    enum Errors: Error {
-        case unableToMap(_ type: String)
-        
-        var localizedDescription: String {
-            switch self {
-            case .unableToMap(let type): 
-                return "Unable to map \(type)"
-            }
-        }
-    }
     
     let decoder: JSONDecoder
     let converter: ResponseJsonConverter?
@@ -30,38 +20,43 @@ class Mapper<T: Decodable> {
     func map(from data: Data?) throws -> T {
         var returnResult: T?
         
-        if let data = data, !data.isEmpty {
-            var finalData = data
-            
-            if let json = try? JSONSerialization.jsonObject(with: data, options: []) {
-                let finalJson = try converter?.convertedJson(from: json) ?? json
-            
-                if let result = finalJson as? T {
-                    returnResult = result
+        do {
+            if let data = data, !data.isEmpty {
+                var finalData = data
+                
+                if let json = try? JSONSerialization.jsonObject(with: data, options: []) {
+                    let finalJson = try converter?.convertedJson(from: json) ?? json
+                
+                    if let result = finalJson as? T {
+                        returnResult = result
+                    }
+                    else {
+                        finalData = try JSONSerialization.data(withJSONObject: finalJson, options: [])
+                    }
                 }
-                else {
-                    finalData = try JSONSerialization.data(withJSONObject: finalJson, options: [])
+                
+                if returnResult == nil {
+                    if let result = (T.self as? DataMappable.Type)?.object(from: data) as? T {
+                        returnResult = result
+                    }
+                    else {
+                        returnResult = try decoder.decode(T.self, from: finalData)
+                    }
                 }
             }
-            
-            if returnResult == nil {
-                if let result = (T.self as? DataMappable.Type)?.object(from: data) as? T {
-                    returnResult = result
-                }
-                else {
-                    returnResult = try decoder.decode(T.self, from: finalData)
-                }
+            else if let result = (T.self as? NoDataMappable.Type)?.noDataObject() as? T {
+                returnResult = result
             }
         }
-        else if let result = (T.self as? NoDataMappable.Type)?.noDataObject() as? T {
-            returnResult = result
+        catch {
+            throw DescriptiveDecodingError(error)
         }
         
         if let result = returnResult {
             return result
         }
         else {
-            throw Errors.unableToMap(String(describing: T.self))
+            throw DescriptiveDecodingError.unableToMap(String(describing: T.self))
         }
     }
 }
