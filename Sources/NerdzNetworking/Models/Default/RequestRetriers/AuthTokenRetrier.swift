3e9c6062834fd2ea6faaee8e381f242abd1f4818
Vasyl Khmil <vasyl@nerdzlab.com>
2024-11-03 13:23:47 +0200

    - added handling of multi requests in token refresh process

--- Sources/NerdzNetworking/Models/Default/RequestRetriers/AuthTokenRetrier.swift
+++ Sources/NerdzNetworking/Models/Default/RequestRetriers/AuthTokenRetrier.swift
@@ -19,6 +19,7 @@ public class AuthTokenRetrier<RequestType: Request>: OnStatusCodesRequestRetrier
     public var onRefreshFailed: RefreshFailedAction?
     
     private var pendingRefreshRequest: RequestType?
+    private var pendingFinishContinuations: [CheckedContinuation<Bool, Never>] = []
     
     public init() {
         
@@ -38,6 +39,14 @@ public class AuthTokenRetrier<RequestType: Request>: OnStatusCodesRequestRetrier
     }
     
     public func handleError<T>(_ error: ErrorResponse<T.ErrorType>, for request: T, on endpoint: Endpoint) async -> T? where T : Request {
+        
+        /// Handling rthe case when one request already started token refresh process. In this case all othher requests just need to wait untill token refresh process will finish instead of starting its own process
+        let refreshHapenned = await waitUntilRefreshFinished()
+        
+        guard !refreshHapenned else {
+            return request
+        }
+        
         guard let refreshRequest = onNeedRefreshRequest?() else {
             return nil
         }
@@ -51,6 +60,12 @@ public class AuthTokenRetrier<RequestType: Request>: OnStatusCodesRequestRetrier
         do {
             let response = try await endpoint.asyncExecute(refreshRequest)
             endpoint.setNewAuthToken(response)
+            
+            /// After refrersh finish we call all pending continuations to specify that refresh finished and no new refresh need to be happenning
+            for continuation in pendingFinishContinuations {
+                continuation.resume(returning: true)
+            }
+            
             return request
         }
         catch ErrorResponse<RequestType.ErrorType>.server(let error, statusCode: let code) {
@@ -64,5 +79,19 @@ public class AuthTokenRetrier<RequestType: Request>: OnStatusCodesRequestRetrier
         }
         
         return nil
+    }
+    
+    /// Retrurning true if process of refreshing is finished and not nedded to be performed
+    private func waitUntilRefreshFinished() async -> Bool {
+        return await withCheckedContinuation { [weak self] continuation in
+            /// If we do not have pending refresh request we return false to specify that refresh need to be happenned
+            if self?.pendingRefreshRequest == nil {
+                continuation.resume(returning: false)
+            }
+            /// In onther case we save continuation to be used after refresh finish
+            else {
+                self?.pendingFinishContinuations.append(continuation)
+            }
+        }
     }
 }
